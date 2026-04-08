66078c626378833b3e07916bd11c6ab810ff0a10
m9henri <missolhenri@gmail.com>
2026-04-08 16:23:31 +0200

    reduced lines in Length.cs

--- Length.cs
+++ Length.cs
@@ -23,120 +23,65 @@ class Length {
         Global.Unit1 = Console.ReadLine();
         
         // checking if the user is smart
-        if (String.IsNullOrEmpty(Global.Unit1))
-        {
-            Environment.Exit(1);
-        }
+        if (String.IsNullOrEmpty(Global.Unit1)) { Environment.Exit(1); }
 
+        Console.Write("You have the following options to convert into:\n\n");
         switch (Global.Unit1)
         {
             case "cm":
                 Console.Write(
-                    "You have the following options to convert into:\n\n" +
-                    "in - inch\n\n" +
-                    ">> "
+                    "in - inch"
                 );
                 break;
             case "in":
                 Console.Write(
-                    "You have the following options to convert into:\n\n" +
                     "cm - centimeter\n" +
-                    "ft - foot\n\n" +
-                    ">> "
+                    "ft - foot"
                 );
                 break;
             case "ft":
                 Console.Write(
-                    "You have the following options to convert into:\n\n" +
                     "cm - centimeter\n" +
                     "in - inch\n" +
                     "m  - meter\n" +
-                    "mi - mile\n\n" +
-                    ">> "
+                    "mi - mile"
                 );
                 break;
             case "m":
                 Console.Write(
-                    "You have the following options to convert into:\n\n" +
-                    "ft - foot\n\n" +
-                    ">> "
+                    "ft - foot"
                 );
                 break;
             case "yd":
                 Console.Write(
-                    "You have the following options to convert into:\n\n" +
                     "ft - foot\n" +
-                    "m  - meter\n\n" +
-                    ">> "
+                    "m  - meter"
                 );
                 break;
             case "km":
                 Console.Write(
-                    "You have the following options to convert into:\n\n" +
-                    "mi - mile\n\n" +
-                    ">> "
+                    "mi - mile"
                 );
                 break;
             case "mi":
                 Console.Write(
-                    "You have the following options to convert into:\n\n" +
                     "ft - foot\n" +
-                    "km - kilometer\n\n" +
-                    ">> "
+                    "km - kilometer"
                 );
                 break;
             default:
                 Environment.Exit(1);
                 break;
         }
+        Console.Write("\n\n>> ");
 
-        Global.Unit2 = Console.ReadLine(); // not checking this
+        Global.Unit2 = Console.ReadLine();
+        if (String.IsNullOrEmpty(Global.Unit2)) { Environment.Exit(1); }
 
         Console.Write($"How many {Global.Unit1} do you wanna convert?\n\n>> ");
         Global.Amount = Convert.ToSingle(Console.ReadLine()); // not checking this like the other, maybe in future update
-        
-        switch ((Global.Unit1, Global.Unit2))
-        {
-            case ("cm", "in"):
-                Global.Result = CentimeterToInch(Global.Amount);
-                break;
-            case ("in", "cm"):
-                Global.Result = InchToCentimeter(Global.Amount);
-                break;
-            case ("in", "ft"):
-                Global.Result = InchToFoot(Global.Amount);
-                break;
-            case ("ft", "cm"):
-                Global.Result = FootToCentimeter(Global.Amount);
-                break;
-            case ("ft", "in"):
-                Global.Result = FootToInch(Global.Amount);
-                break;
-            case ("ft", "m"):
-                Global.Result = FootToMeter(Global.Amount);
-                break;
-            case ("ft", "mi"):
-                Global.Result = FootToMile(Global.Amount);
-                break;
-            case ("m", "ft"):
-                Global.Result = MeterToFoot(Global.Amount);
-                break;
-            case ("yd", "ft"):
-                Global.Result = YardToFoot(Global.Amount);
-                break;
-            case ("yd", "m"):
-                Global.Result = YardToMeter(Global.Amount);
-                break;
-            case ("km", "mi"):
-                Global.Result = KilometerToMile(Global.Amount);
-                break;
-            case ("mi", "ft"):
-                Global.Result = MileToFoot(Global.Amount);
-                break;
-            case ("mi", "km"):
-                Global.Result = MileToKilometer(Global.Amount);
-                break;
-        }
+
+        Converter(Global.Unit1, Global.Unit2, Global.Amount);
 
         if (Global.Amount == 1)
         {
@@ -149,57 +94,21 @@ class Length {
         Console.Write("\n\nPress any key to close the program. . . ");
         Console.ReadKey();
     }
-
-    static double CentimeterToInch(float input)
-    {
-        return input * 0.3937008;
-    }
-    static double InchToCentimeter(float input)
-    {
-        return input * 2.54;
-    }
-    static double InchToFoot(float input)
-    {
-        return input * 0.08333333;
-    }
-    static double FootToCentimeter(float input)
-    {
-        return input * 30.48;
-    }
-    static double FootToInch(float input)
-    {
-        return input * 12;
-    }
-    static double FootToMeter(float input)
-    {
-        return input * 0.3048;
-    }
-    static double FootToMile(float input)
-    {
-        return input * 0.0001893939;
-    }
-    static double MeterToFoot(float input)
-    {
-        return input * 3.28084;
-    }
-    static double YardToFoot(float input)
-    {
-        return input * 3;
-    }
-    static double YardToMeter(float input)
-    {
-        return input * 0.9144;
-    }
-    static double KilometerToMile(float input)
-    {
-        return input * 0.6213712;
-    }
-    static double MileToFoot(float input)
-    {
-        return input * 5280;
-    }
-    static double MileToKilometer(float input)
-    {
-        return input * 1.609344;
-    }
+    
+    static double Converter(string unit1, string unit2, float amount) => (unit1, unit2) switch
+        {
+            ("cm", "in") => amount * 0.3937008,
+            ("in", "cm") => amount * 2.54,
+            ("in", "ft") => amount * 0.08333333,
+            ("ft", "cm") => amount * 30.48,
+            ("ft", "in") => amount * 12,
+            ("ft", "m") => amount * 0.3048,
+            ("ft", "mi") => amount * 0.0001893939,
+            ("m", "ft") => amount * 3.28084,
+            ("yd", "ft") => amount * 3,
+            ("yd", "m") => amount * 0.9144,
+            ("km", "mi") => amount * 0.6213712,
+            ("mi", "ft") => amount * 5280,
+            ("mi", "km") => amount * 1.609344
+        };
 }
